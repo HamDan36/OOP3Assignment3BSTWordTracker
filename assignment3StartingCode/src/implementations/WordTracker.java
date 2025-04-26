211f07f7dec67be6a1eb6fbc2bb97e48f3f16910
HamDan36 <150320417+HamDan36@users.noreply.github.com>
2025-04-25 23:16:46 -0600

    adds documentation

--- assignment3StartingCode/src/implementations/WordTracker.java
+++ assignment3StartingCode/src/implementations/WordTracker.java
@@ -1,3 +1,10 @@
+/**
+ * WordTracker is a class that implements BSTree and helper method WordTrackerData, it loads, save a BST based on the user input from the command line and creates BST from the input text file. Depending on the input, it can output the frequency and where the words in the text file.
+ * 
+ * @author Daniel Chu
+ * @version 1.0
+ */
+
 package implementations;
 import java.io.BufferedReader;
 import java.io.File;
@@ -13,12 +20,20 @@ import java.io.Serializable;
 
 import implementations.BSTreeNode;
 
+
 public class WordTracker implements Serializable
 {
 	private static final long serialVersionUID = 2025L;
 	
 	BSTree<WordTrackerData> wordTree = new BSTree<>();
 	
+	/**
+	 * Processes the input file, parses through it line by line to extract the words and log their occurrences into an ArrayList, and the line it appears on
+	 * Preconditions: A valid WordTracker Object must exist, user inputs must be valid
+	 * Postconditions: words from the input file are placed into a ArrayList
+	 * 
+	 * @param filePath Relative path of the input file
+	 */
 	public void processFile(String filePath)
 	{
 		String path = "./res/";
@@ -53,11 +68,16 @@ public class WordTracker implements Serializable
 		}
 	}
 
-//	public String getWord()
-//	{
-//		return word;
-//	}
-	
+
+	/**
+	 * Updates the frequency of a word from the input text file. if the word was not in the BST, it will start a new count for the word
+	 * Preconditions: a valid WordTracker Object must exist, word cannot be null or empty or blank
+	 * Postconditions: the frequency of the word from the text file is updated
+	 * 
+	 * @param word Word from the text file
+	 * @param fileName Name of the input file
+	 * @param lineNum Number of the line from where the word was extracted
+	 */
 	public void freqCounter(String word, String fileName, int lineNum)
 	{
 		WordTrackerData temp = new WordTrackerData(word);
@@ -76,6 +96,14 @@ public class WordTracker implements Serializable
 		}
 	}
 	
+	/**
+	 * Converts a line of from input file into an ArrayList of words without the punctuation
+	 * Preconditions: a valid WordTracker Object must exist. A valid input file and a non-null line from the file must exist.  
+	 * Postconditions: an ArrayList Object from containing all the words from one line of text is created
+	 * 
+	 * @param line A line of words from the input text
+	 * @return Returns an ArrayList containing the words from a line of text
+	 */
 	private ArrayList<String> makeList(String line)
 	{
 		ArrayList<String> wordList = new ArrayList<String>();
@@ -104,7 +132,14 @@ public class WordTracker implements Serializable
 		return wordList;
 	}
 	
-	
+	/**
+	 * Processes a single word, strips all unwanted characters and whitespace, converts it to lowercase
+	 * Preconditions: a valid WordTracker Object must exist. The word input cannot be null
+	 * Postconditions: returns the word without the unwanted characters in lowercase
+	 * 
+	 * @param word Word from the line of text
+	 * @return Returns the word without the unwanted characters in lowercase
+	 */
 	private String MicrosoftWord(String word) //word processor
 	{
 		word = word.replaceAll(",.?/!\";:()", "").toLowerCase();
@@ -113,6 +148,13 @@ public class WordTracker implements Serializable
 		return word;
 	}
 	
+	/**
+	 * main method of the WordTracker class
+	 * Preconditions: none
+	 * Postconditions: the main program of WordTracker is run
+	 * 
+	 * @param args Input arguments from the CLI from the user
+	 */
 	public static void main(String[] args)
 	{
 		WordTracker treeTracker = new WordTracker();
@@ -120,7 +162,13 @@ public class WordTracker implements Serializable
 		treeTracker.handleUser(args);
 	}
 	
-
+	/**
+	 * Saves the current BST WordTracker Object into specified file
+	 * Preconditions: a valid WordTracker Object must exist. A valid filename must be input from the user
+	 * Postconditions: A BST WordTracker Object is serialized and saved into the specified file
+	 * 
+	 * @param fileName Name of the file where the WordTracker Object is saved.
+	 */
 	public void saveTree(String fileName) 
 	{		
 		try (ObjectOutputStream save = new ObjectOutputStream(new FileOutputStream(fileName))) 
@@ -134,7 +182,13 @@ public class WordTracker implements Serializable
 		}
 	}
 	
-	
+	/**
+	 * Checks for an existing saved BST WordTracker Object in and loads the BST if one is found
+	 * 
+	 * Preconditions: none
+	 * Postconditions: a WordTracker Object is loaded from the save file if one is found
+	 * 
+	 */
 	public void checkSave()
 	{		
 		File savedFile = new File("./testOutput/repository.ser");
@@ -152,7 +206,13 @@ public class WordTracker implements Serializable
 		}
 	}
 	
-	
+	/**
+	 * Loads the BST WordTracker Object from a serialized file
+	 * 
+	 * Preconditions: a valid serialized WordTracker Object must exist in the specified file
+	 * Postconditions: a BST WordTracker Object is loaded in with the data from the file
+	 * 
+	 */
 	public void loadTree() {
 		try (ObjectInputStream load = new ObjectInputStream(new FileInputStream("./testOutput/repository.ser"))) 
 		{
@@ -165,6 +225,13 @@ public class WordTracker implements Serializable
 		}
 	}
 	
+	/**
+	 * Handles the user inputs, processes input file and saved file and saves the file based on user inputs
+	 * Preconditions: a valid WordTracker Object must exist
+	 * Postconditions: a BST WordTracker Object is created and output is printed based on the user input
+	 * 
+	 * @param args CLI arguments from the user containing the input and output file and the type of processing to be displayed
+	 */
 	public void handleUser(String[] args)
 	{
 		if (args.length < 2)
@@ -220,22 +287,32 @@ public class WordTracker implements Serializable
 				System.out.print(output.toString());
 			}
 
-		saveTree("./testOutput/repository.ser"); 
+		if(outputFile != null)
+		{
+//			System.out.println("if output fike is not null" + outputFile);
+			saveTree(outputFile);
+		}
+		
+		else
+		{
+			saveTree("./testOutput/repository.ser");
+		}
 	}
 	
-	public void printTree() // testing
-	{
-		if (wordTree.isEmpty())
-		{
-			System.out.println("Tree has no words");
-			return;
-		}
-		
-		utilities.Iterator<WordTrackerData> iterator = wordTree.inorderIterator();
-		
-		while(iterator.hasNext())
-		{
-			System.out.println(iterator.next());
-		}
-	}
+	
+//	public void printTree() // testing
+//	{
+//		if (wordTree.isEmpty())
+//		{
+//			System.out.println("Tree has no words");
+//			return;
+//		}
+//		
+//		utilities.Iterator<WordTrackerData> iterator = wordTree.inorderIterator();
+//		
+//		while(iterator.hasNext())
+//		{
+//			System.out.println(iterator.next());
+//		}
+//	}
 }
--- assignment3StartingCode/src/implementations/WordTrackerData.java
+++ assignment3StartingCode/src/implementations/WordTrackerData.java
@@ -1,3 +1,9 @@
+/**
+ * Helper method to the WordTracker class
+ * 
+ * @author Daniel Chu
+ * @version 1.0
+ */
 package implementations;
 import java.io.Serializable;
 import java.util.*;
@@ -12,6 +18,13 @@ public class WordTrackerData implements Comparable<WordTrackerData>, Serializabl
 	
 	private Map<String, Set<Integer>> occurence;
 	
+	/**
+	 * default parameterized method for the WordTrackerData class
+	 * Preconditions: none
+	 * Postconditions: A new WordTrackerData Object is created
+	 * 
+	 * @param word Word to be tracked for frequency and occurrence
+	 */
 	public WordTrackerData(String word)
 	{
 		this.word = word.toLowerCase(); //done twice just in case
@@ -19,6 +32,14 @@ public class WordTrackerData implements Comparable<WordTrackerData>, Serializabl
 		this.occurence = new HashMap<>();
 	}
 	
+	/**
+	 * Increments the frequency and updates the occurrence of the tracked word
+	 * Preconditions: a valid WordTrackerData Object must exist, a valid input file with valid lines must exist
+	 * Postconditions:  the frequency and occurrence of the word is updated
+	 * 
+	 * @param fileName Name of the input file
+	 * @param lineNum Number of the line where the word was extracted
+	 */
 	public void addOccuence(String fileName, int lineNum)
 	{
 		freq++;
@@ -27,21 +48,49 @@ public class WordTrackerData implements Comparable<WordTrackerData>, Serializabl
 		occurence.get(fileName).add(lineNum); //adds line num to file
 	}
 	
+	/**
+	 * Getter for frequency of the WordTrackerData Object
+	 * Preconditions: a valid WordTrackerData Object must exist
+	 * Postconditions: the frequency of the WordTrackerData Object is returned
+	 * 
+	 * @return Returns the frequency of the WordTrackerData Object 
+	 */
 	public int getFreq()
 	{
 		return freq;
 	}
 	
+	/**
+	 * Getter method of word in the WordTrackerData Object 
+	 * Preconditions: a valid WordTrackerData Object  must exist
+	 * Postconditions: the word from the WordTrackerData Object is returned
+	 * 
+	 * @return Returns the word from the WordTrackerData Object 
+	 */
 	public String getWord()
 	{
 		return word;
 	}
 	
+	/**
+	 * Getter method for the occurrence map of the WordTrackerData Object 
+	 * Preconditions: a valid WordTrackerData Object must exist
+	 * Postconditions: the occurrence map of the WordTrackerData Object is returned
+	 * 
+	 * @return Returns the occurrence map of the WordTrackerData Object 
+	 */
 	public Map<String, Set<Integer>> getOccurence()
 	{
 		return occurence;
 	}
 	
+	/**
+	 * Prints a string listing the word and which file it was from
+	 * Preconditions: a valid WordTrackerData Object must exist
+	 * Postconditions: a string listing the word and the file it was from is displayed
+	 * 
+	 * @return Returns a string containing the word and the file it was from 
+	 */
 	public String printFilesOnly() //-pf
 	{
 		StringBuilder sb = new StringBuilder();
@@ -56,6 +105,13 @@ public class WordTrackerData implements Comparable<WordTrackerData>, Serializabl
 		return sb.toString().trim();
 	}
 
+	/**
+	 * Prints a string listing the word and which file and the line number it was from
+	 * Preconditions: a valid WordTrackerData Object must exist
+	 * Postconditions: a string listing the word and the file and the line number it was from is displayed
+	 * 
+	 * @return Returns a string containing the word and the file and the line number it was from 
+	 */
 	public String printFilesLines() //-pl
 	{
 		StringBuilder sb = new StringBuilder();
@@ -80,6 +136,13 @@ public class WordTrackerData implements Comparable<WordTrackerData>, Serializabl
 		return sb.toString().trim();
 	}
 
+	/**
+	 * Prints a string listing the word, file, line number it was from along with the frequency
+	 * Preconditions: a valid WordTrackerData Object must exist
+	 * Postconditions: a string listing the word, file, line number it was from along with the frequency it was from is displayed
+	 * 
+	 * @return Returns a string containing the word, file, line number it was from along with the frequency 
+	 */
 	public String printFilesLinesFrequency()  //-po
 	{
 		StringBuilder sb = new StringBuilder();
